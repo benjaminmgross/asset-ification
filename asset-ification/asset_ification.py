f3d5c7ebfbe0ca05a78472d10bee76bae84d2728
benjaminmgross <benjaminmgross@yahoo.com>
2014-06-13 05:25:00 -0400

    Added the construction of the master ETF list from price-data website

--- asset-ification/asset_ification.py
+++ asset-ification/asset_ification.py
@@ -25,6 +25,61 @@ import numpy
 def scipt_function(arg_1, arg_2):
 	return None
 
+def calibrate_model():
+    """
+    Price data has all of the tickers of ETFs and descriptions available
+    on its website.  That's where the 'aggregate list' in `./dat`
+    comes from
+    """
+    return None
+
+def gen_etf_list(path):
+    """
+    www.price-data.com has pages with all of the etf tickers
+    available on it.  This function accesses each of those pages,
+    stores the values to :class:`pandas.DataFrame` and saves to
+    file
+
+    :ARGS:
+    
+        path: :class:`string` of the path to save the :class:`DataFrame`
+        of tickers
+    """
+
+    #the first three url's from price-data, the rest can be generated
+    p_a = "http://www.price-data.com/listing-of-exchange-traded-funds/"
+    p_b = "http://www.price-data.com/listing-of-exchange-traded-funds/listing-of-exchange-traded-funds-starting-with-b/"
+    p_c = "http://www.price-data.com/listing-of-exchange-traded-funds/list-of-exchange-traded-funds-starting-with-c/"
+    p_k = "http://www.price-data.com/listing-of-exchange-traded-funds/list-of-exchanged-traded-funds-starting-with-k/"
+
+    base_url = "http://www.price-data.com/listing-of-exchange-traded-funds/list-of-exchange-traded-funds-starting-with-"
+    letters = 'defghijlmnopqrstuvwxyz'
+    url_dict = dict(map(lambda x, y: [x, y], letters,
+        map(lambda s: base_url + s, letters)))
+
+    #the wonky ones
+    url_dict['a'], url_dict['b'], url_dict['c'] = p_a, p_b, p_c
+    url_dict['k'] = p_k
+
+    #pull down the tables into a list
+
+    d =[]
+    for letter in url_dict.keys():
+        try:
+            d.extend(pandas.read_html(url_dict[letter], index_col = 0, infer_types = False))
+            print "succeeded for letter " + letter
+        except:
+            print "Did not succeed for letter " + letter
+
+    agg_df = pandas.concat(d, axis = 0)
+    agg_df.columns = ['Description']
+    agg_df.index.name = 'Ticker'
+
+    agg_df.to_csv(path)
+    return None
+    
+    
+    
 if __name__ == '__main__':
 	
 	usage = sys.argv[0] + "usage instructions"
